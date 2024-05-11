8b434134cdad64dcc0982e9a146f4705fde8a94c
t0f <toffelaakso@gmail.com>
2024-05-11 19:39:15 +0300

    added Flaw 1.

--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -3,6 +3,7 @@ from .models import *
 from .forms import *
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
+from django.db import connection #for the unsafe db insertion
 
 # Create your views here.
 def index(request):
@@ -13,13 +14,33 @@ def todo(request):
     todos = Todo.objects.filter(created_by=request.user)
     form = TodoForm()
 
+    #1. A03:2021-Injection. Unsafe way
     if request.method == 'POST':
-        form = TodoForm(request.POST)
-        todo = form.save(commit=False)
-        todo.created_by = request.user
-        if form.is_valid():
-            form.save()
-            return redirect('todo')
+
+        # If you give as todo's title the following:
+        # 1','0','2'); DROP TABLE todo_todo; --
+        # it will drop the table that has all the todos rendering the app unusable.
+        
+        # The following input:
+        # 1','0','2'); UPDATE todo_todo SET title='hacked' WHERE 1 = 1; --
+        # changes all task's titles to 'hacked'. 
+        # This is a safer way to see that injection is possible, without completely destroying the app.
+
+        cursor = connection.cursor()        
+        raw_title = request.POST.get('title','')
+        raw_query = "INSERT INTO todo_todo (title, completed, created_by_id) VALUES ('%s', '0', '2')" % (raw_title)
+        cursor.executescript(raw_query)
+
+        return redirect('todo')
+
+    #1. A03:2021-Injection. Safe way / fix.
+    #if request.method == 'POST':
+    #    form = TodoForm(request.POST)
+    #    todo = form.save(commit=False)
+    #    todo.created_by = request.user
+    #    if form.is_valid():
+    #        form.save()
+    #        return redirect('todo')
     
     return render(request, 'todos.html', {'todos':todos, 'form':form})
 
